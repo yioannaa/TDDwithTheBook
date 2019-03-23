90b1870f8663c259897e49280244ad39b70f972e
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-03-23 23:26:23 +0100

    adding assertj (instead of junit)

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -2,21 +2,37 @@ package com.packtpublishing.tddjava.ch03tictactoe;
 
 public class TicTacToe {
 
-    private Character [][] board = {{null, null, null}, {null, null, null}, {null, null, null}};
+    private Character[][] board = {{null, null, null}, {null, null, null}, {null, null, null}};
+    private char lastPlayer = '\0';
 
-    public void play(int x, int y){
-        if (x<1 || x>3){
-            throw new RuntimeException("X is outside board");
-        }else if (y<1 || y>3){
-            throw new RuntimeException("Y is outside board");
-        }
-
-        if (board [x-1][y-1] != null ) {
-            throw new RuntimeException("Is already occupied");
-        }else {
-            board [x-1][y-1] = 'X';
-        }
-
+    public void play(int x, int y) {
+        check(x);
+        check(y);
+        setBoard(x,y);
+        lastPlayer = nextPlayer();
     }
+
+
+    public void check(int axis) {
+        if (axis < 1 || axis > 3) {
+            throw new RuntimeException("Is outside the board");
+        }
+    }
+
+    public void setBoard(int x, int y) {
+        if (board[x - 1][y - 1] != null) {
+            throw new RuntimeException("Is already occupied");
+        } else {
+            board[x - 1][y - 1] = 'X';
+        }
+    }
+
+    public char nextPlayer(){
+         if (lastPlayer == 'X'){
+             return 'O';
+         }
+         return 'X';
+    }
+
 
 }
--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -4,6 +4,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 
 public class TicTacToeSpec {
@@ -19,21 +22,51 @@ public class TicTacToeSpec {
 
     @Test
     public void whenXOutsideBoardRuntimeException(){
-        exception.expect(RuntimeException.class);
-        ticTacToe.play(5,2);
+//        exception.expect(RuntimeException.class);
+        //when
+//        ticTacToe.play(5,2);
+        //then
+        assertThatThrownBy(()-> {
+            ticTacToe.play(5,2);
+        }).isInstanceOf(RuntimeException.class);
+
     }
 
     @Test
     public void whenYOutsideBoardRuntimeException(){
-        exception.expect(RuntimeException.class);
-        ticTacToe.play(2,5);
+        assertThatThrownBy(()->{
+            ticTacToe.play(2,5);
+        }).isInstanceOf(RuntimeException.class);
+//        exception.expect(RuntimeException.class);
+//        ticTacToe.play(2,5);
     }
 
     @Test
     public void whenOccupiedRuntimeException(){
         ticTacToe.play(2,1);
-        exception.expect(RuntimeException.class);
-        ticTacToe.play(2,1);
+        assertThatThrownBy(()-> {
+            ticTacToe.play(2,1);
+        }).isInstanceOf(RuntimeException.class);
+//        ticTacToe.play(2,1);
+//        exception.expect(RuntimeException.class);
+//        ticTacToe.play(2,1);
+    }
+
+    @Test
+    public void XshouldBeGivnFirstTurnAndAfterO(){
+//        assertEquals('X', ticTacToe.nextPlayer());
+        assertThat(ticTacToe.nextPlayer()).isEqualTo('X');
+    }
+
+    @Test
+    public void OshouldBeLastAndAfterX(){
+
+        //when
+        ticTacToe.play(1,1);
+
+        //then
+//        assertEquals('O', ticTacToe.nextPlayer());
+        assertThat(ticTacToe.nextPlayer()).isEqualTo('O');
     }
 
 
